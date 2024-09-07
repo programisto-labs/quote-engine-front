65268f77f3489b3a40eddc06edb7c166dbb3f645
Tech <tech@programisto.fr>
2024-09-07 13:10:14 -0300

    Setting production on develop

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   corsOrigin: 'https://develop-sr3snxi-d7qxrnyoqcw3u.eu-5.platformsh.site',
   devisRapideApiUrl: 'https://develop-sr3snxi-d7qxrnyoqcw3u.eu-5.platformsh.site/devis'
 };
