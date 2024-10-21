6c7659a382204bafc32cdd1590a74e463bd721b4
Tech <tech@programisto.fr>
2024-10-21 14:54:05 -0300

    Deleting preloader

--- src/app/core/bootstrap/preloader.service.ts
+++ src/app/core/bootstrap/preloader.service.ts
@@ -8,22 +8,7 @@ export class PreloaderService {
   private readonly platform = inject(PLATFORM_ID);
   private readonly document = inject(DOCUMENT);
 
-  private readonly selector = 'globalLoader';
-
-  private getElement() {
-    return isPlatformBrowser(this.platform) ? this.document.getElementById(this.selector) : undefined;
-  }
-
   hide() {
-    const el = this.getElement();
-    if (el) {
-      el.addEventListener('transitionend', () => {
-        el.className = 'global-loader-hidden';
-      });
-
-      if (!el.classList.contains('global-loader-hidden')) {
-        el.className += ' global-loader-fade-out';
-      }
-    }
+    console.log('Preloading...');
   }
 }
