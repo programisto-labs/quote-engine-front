3ce890daf51e75d790d56af483b45abffe761214
Tech <tech@programisto.fr>
2024-10-22 17:35:38 -0300

    Not sending discord message, just workflow webhook message

--- src/app/shared/services/discord.service.ts
+++ src/app/shared/services/discord.service.ts
@@ -1,6 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {concatAll, Observable, takeLast} from 'rxjs';
-import {fromArrayLike} from "rxjs/internal/observable/innerFrom";
+import {Observable, takeLast} from 'rxjs';
 import {Autocomplete} from "./devis.service";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
@@ -16,29 +15,14 @@ export class DiscordService {
   devisRapideApiUrl = environment.devisRapideApiUrl;
 
   sendNotificationMessages(contactData: ClientContact, devis: any, projet: any) {
-    let discord_message = this.buildDiscordData(contactData, devis, projet, environment.discord_webhook);
     let workflow_message = this.buildDiscordRawData(contactData, devis, projet, environment.workflow_webhook);
-
-
-    fromArrayLike([
-      this.sendDiscordRawMessage(workflow_message),
-      this.sendDiscordMessage(discord_message)
-    ]).pipe(
-      concatAll(),
+    this.sendDiscordRawMessage(workflow_message).pipe(
       takeLast(1)
     ).subscribe({
       error: (e) => {
         console.log('DiscordService: sendNotificationMessages ' + e);
       }
     });
-  }
-
-  private buildDiscordData(contact: ClientContact, devis: any, projet: any, webhook: string) {
-    return {
-      content: `Le client ${contact.fullname} a envoyé un projet\\ndans sa boîte mail (${contact.email})!!!`,
-      embeds: this.discordDatatableBuilderService.buildDiscordTable(devis as Devis, projet),
-      webhook
-    }
   }
 
   private buildDiscordRawData(contact: any, devis: any, projet: any, webhook: string) {
@@ -50,9 +34,9 @@ export class DiscordService {
     }
   }
 
-  public sendDiscordMessage(discordData: any): Observable<Autocomplete> {
+  /*public sendDiscordMessage(discordData: any): Observable<Autocomplete> {
     return this.http.post<any>(`${this.devisRapideApiUrl}/discord/webhook/send`, discordData, {headers: {'Content-Type': 'application/json',}});
-  }
+  }*/
 
   public sendDiscordRawMessage(discordData: any): Observable<Autocomplete> {
     return this.http.post<any>(`${this.devisRapideApiUrl}/discord/webhook/sendRaw`, discordData, {headers: {'Content-Type': 'application/json',}});
