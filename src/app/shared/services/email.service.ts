78b24683ef96a40edc04627e2fe8c27a341e939d
Tech <tech@programisto.fr>
2024-10-23 14:57:47 -0300

    Sending email just to cient

--- src/app/shared/services/email.service.ts
+++ src/app/shared/services/email.service.ts
@@ -1,7 +1,6 @@
 import {inject, Injectable} from '@angular/core';
-import {BehaviorSubject, concatAll, delay, Observable, takeLast} from 'rxjs';
+import {BehaviorSubject, Observable, takeLast} from 'rxjs';
 import {ToastrService} from "ngx-toastr";
-import {fromArrayLike} from "rxjs/internal/observable/innerFrom";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 
@@ -17,15 +16,8 @@ export class EmailService {
   sendNotificationMessages(clientEmail: string, clientName: string, devis: any, projet: any) {
     this.sendingEmailStatus.next('sending');
     const clientData = this.buildClientData(clientEmail, clientName, devis, projet);
-    const salesData = this.buildSalesData(clientEmail, clientName, devis, projet);
 
-    fromArrayLike([
-      this.sendEmail(clientData).pipe(delay(5000)),
-      this.sendEmail(salesData)
-    ]).pipe(
-      concatAll(),
-      takeLast(1)
-    ).subscribe({
+    this.sendEmail(clientData).pipe(takeLast(1)).subscribe({
       next: _ => {
         this.toastService.success('L\'e-mail a été envoyé avec succès.');
         this.sendingEmailStatus.next('sent');
@@ -35,16 +27,6 @@ export class EmailService {
         this.toastService.error('Une erreur s\'est produite lors de l\'envoi de l\'e-mail. Veuillez réessayer.');
       }
     });
-  }
-
-  private buildSalesData(clientEmail: string, clientName: string, devis: any, projet: any) {
-    return {
-      clientEmail: environment.salesEmail,
-      clientName: "Sales team",
-      subject: `Prospect envoyé quote (nom: ${clientName}, email: ${clientEmail})`,
-      devis,
-      projet
-    }
   }
 
   private buildClientData(clientEmail: string, clientName: string, devis: any, projet: any) {
